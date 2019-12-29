d1c0790cf04641d7bd52b3ed2c161341fb871f4b
Aaron Allen <aaron@flipstone.com>
2019-12-29 00:58:32 -0600

    eighteen

--- src/Day/Eighteen.hs
+++ src/Day/Eighteen.hs
@@ -2,17 +2,19 @@
 module Day.Eighteen where
 
 import           Control.Applicative (liftA2)
-import           Control.Arrow ((&&&))
+import           Control.Arrow ((&&&), first, second)
 import           Control.Monad (join)
 import           Control.Monad.Trans.Cont
 import           Control.Monad.Trans.State
 import           Data.Array
 import qualified Data.ByteString.Char8 as BS8
 import           Data.Char (isLower, isUpper, toLower)
+import           Data.List (sort)
 import qualified Data.Map as M
 import           Data.Maybe (fromMaybe, mapMaybe)
 import           Data.Semigroup (Min(..))
 import qualified Data.Set as S
+import           Data.Traversable (for)
 import           Safe (lastMay)
 
 import           Debug.Trace
@@ -20,39 +22,53 @@ import           Debug.Trace
 dayEighteenA :: BS8.ByteString -> BS8.ByteString
 dayEighteenA inp = fromMaybe "invalid input" $ do
   (maze, keys, start) <- parseMaze inp
-  result <- evalState (collectKeys maze (S.fromList keys) ('@', start) S.empty 0) (M.empty, maxBound)
+  let keyToKey = M.fromList
+        [ ((k1, k2), (d, ks))
+        | x@(k1, c1) <- keys
+        , (k2, c2) <- dropWhile (<= x) keys
+        , Just (d, ks) <- [shortestWithKey maze c1 c2]
+        ]
+
+  result <- evalState (collectKeys maze keyToKey (S.fromList keys) ('@', start) S.empty 0) (maxBound, M.empty)
   pure . BS8.pack $ show result
 
 -- could make this tail recursive and put the current min in state.
-collectKeys :: Maze -> S.Set KeyLoc -> (Char, (Int, Int)) -> S.Set Char -> Int -> State (Distances, Int) (Maybe Int)
-collectKeys _ k _ _ acc | S.null k acc = pure $ Just acc
-collectKeys maze keysToGo (curKey, pos) keysInv acc = do
-  stop <- modify ((< acc) . snd)
-  if stop
-     then pure Nothing
-     else fmap getMin . mconcat <$> traverse collectKey (S.toList keysToGo)
+collectKeys :: Maze -> KeyToKey -> S.Set KeyLoc -> (Char, (Int, Int)) -> S.Set Char -> Int -> State (Int, Distance) (Maybe Int)
+collectKeys _ _ k _ _ acc | S.null k = traceShow acc $ Just acc <$ modify (first $ const acc)
+collectKeys maze keyToKey keysToGo (curKey, pos) keysInv acc =
+  fmap getMin . mconcat <$> traverse collectKey (S.toList keysToGo)
   where
-    collectKey :: KeyLoc -> State Distances (Maybe (Min Int))
+    collectKey :: KeyLoc -> State (Int, Distance) (Maybe (Min Int))
     collectKey targetKey@(k, kc) = do
-      dp <- get
       let fstKey = min curKey k
           sndKey = max curKey k
-      case join $  M.lookup sndKey <$> M.lookup (fstKey, keysInv) dp of
-        Just x -> pure $ Min <$> x
+          invKeyList = S.toList keysInv
+      dp <- gets snd
+      mbD <- case M.lookup (fstKey, sndKey, invKeyList) dp of
         Nothing -> do
-          r <- case shortestDistance maze keysInv pos kc of
-                  Nothing -> pure Nothing
-                  b@(Just xx) -> do
-                    collectKeys maze (S.delete targetKey keysToGo) targetKey (S.insert k keysInv) (acc + xx)
-                    pure b
+          let mbDist = case M.lookup (fstKey, sndKey) keyToKey of
+                         Just (dist, req)
+                           | req `S.isSubsetOf` keysInv -> Just dist
+                         _ -> shortestDistance maze keysInv pos kc
+          modify . second $ M.insert (fstKey, sndKey, invKeyList) mbDist
+          pure mbDist
+        Just x -> pure x
 
-          modify $ M.insert (fstKey, keysInv) (M.singleton sndKey r)
-          pure $ Min <$> r
+      r <- for mbD $ \d -> do
+             curMin <- gets fst
+             let acc' = acc + d
+             if acc' >= curMin
+                then pure Nothing
+                else do
+                  collectKeys maze keyToKey (S.delete targetKey keysToGo) targetKey (S.insert k keysInv) $! acc'
 
--- Stores shortest distance results from one key to another.
--- has the list of keys needed for the route.
-type Distances =
-  M.Map (Char, S.Set Char) (M.Map Char (Maybe Int))
+      pure $ Min <$> join r
+
+type KeyToKey = M.Map (Char, Char) (Int, S.Set Char)
+
+type Distance = M.Map (Char, Char, String) (Maybe Int)
+
+type DP = M.Map (Char, String) (Maybe Int)
 
 type Maze = Array (Int, Int) Tile
 
@@ -68,7 +84,7 @@ parseMaze bs = do
     ((w, h), _) <- lastMay cells
     let keys = mapMaybe getKey cells
     [start] <- Just $ mapMaybe getStart cells
-    pure (array ((0, 0), (w, h)) cells, keys, start)
+    pure (array ((0, 0), (w, h)) cells, sort keys, start)
   where
     getKey (coord, Key c) = Just (c, coord)
     getKey _ = Nothing
@@ -118,3 +134,29 @@ shortestDistance maze keys start end = go [start] [end] M.empty M.empty 0 where
 canOpenDoor :: S.Set Char -> Char -> Bool
 canOpenDoor keys door = toLower door `S.member` keys
 
+
+shortestWithKey :: Maze -> (Int, Int) -> (Int, Int) -> Maybe (Int, S.Set Char)
+shortestWithKey maze start end = go [(start, S.empty)] [(end, S.empty)] M.empty M.empty 0 where
+  go [] _ _ _ _ = Nothing
+  go _ [] _ _ _ = Nothing
+  go sq eq sv ev acc = (`runCont` id) . callCC $ \k -> do
+    let sv' = foldr (\(c, ks) -> M.insert c (acc, ks)) sv sq
+        ev' = foldr (\(c, ks) -> M.insert c (acc, ks)) ev eq
+        search visited dest (c@(x, y), keys)
+          | Just (d, ks) <- M.lookup c dest = k . Just $ (acc + d, keys <> ks)
+          | otherwise = pure
+              [ (coord, keys <> key)
+              | coord <- [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+              , not $ M.member coord visited
+              , case maze ! coord of
+                  Empty _ -> True
+                  Wall -> False
+                  Door c -> True
+                  Key _ -> True -- track keys that are picked up on the way?
+              , let key = case maze ! coord of
+                            Door c -> S.singleton c
+                            _ -> S.empty
+              ]
+    sq' <- S.toList . S.fromList . concat <$> traverse (search (fst <$> sv') ev') sq
+    eq' <- S.toList . S.fromList . concat <$> traverse (search (fst <$> ev') sv') eq
+    pure . go sq' eq' sv' ev' $! acc + 1
